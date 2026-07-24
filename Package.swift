8be5b9ef7b1f073b99b4fa59b25b046d0351229c
Benedikt <91166910+bndkt@users.noreply.github.com>
2026-07-24 14:51:27 +0200

    Rename repository and always build native artifacts

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let hermesVM: Target = if let localArtifact {
 } else {
   .binaryTarget(
     name: "HermesVM",
-    url: "https://github.com/renkudev/renku-native-runtime/releases/download/0.1.1/hermesvm.xcframework.zip",
+    url: "https://github.com/renkudev/renku-native/releases/download/0.1.1/hermesvm.xcframework.zip",
     checksum: "b71af5cd41a951640caa5afebbd2e98a5278554860e5de8b81a3b0737cf22b2c"
   )
 }
